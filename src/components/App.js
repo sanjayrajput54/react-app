f3c8cd6a3a384849b9afe77710cf28889e1619c3
Sanjay Rajput <sanjayrajput54@yahoo.com>
2023-09-04 22:01:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import AddTodo from "../containers/AddTodo";
 import VisibleTodoList from "../containers/VisibleTodoList";
 
 const App = () => (
-  <div style={{display:"flex", "justify-content":"center}}>
+  <div style={{"display":"flex", "justify-content":"center"}}>
   <div>
     <AddTodo />
     <VisibleTodoList />
